3b49edf5d4c94497d16b4420e30d08273322cefe
sudarat2538 <sudaratthongkaew2538@gmail.com>
2018-02-12 15:45:39 +0700

    Databesd connceting

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,25 @@
 package sample;
 
-public class Controller {
+import com.sun.org.apache.bcel.internal.generic.FMUL;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    private loginModel loginmodel = new loginModel();
+    @FXML
+    private Label dbStatus;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        if (this.loginmodel.isDatabaseConnection()) {
+            this.dbStatus.setText("Connected to DB");
+    }else{
+        this.dbStatus.setText("Not Connected to DB");
+    }
+    }//initialize
 }
