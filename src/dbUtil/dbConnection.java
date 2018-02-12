ae891c154a19cfe8d73961181f08dae9a045ab69
sudarat2538 <sudaratthongkaew2538@gmail.com>
2018-02-12 16:51:39 +0700

    User Login with sqlite

--- src/dbUtil/dbConnection.java
+++ src/dbUtil/dbConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class dbConnection {
-    private static final String SQCONN = "jdbc:sqlite:school.sqlite";
+    private static final String SQCONN = "jdbc:sqlite:\\school.sqlite";
 
     public static Connection getConnection() throws SQLException {
         try {
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,10 +1,19 @@
 package sample;
 
 import com.sun.org.apache.bcel.internal.generic.FMUL;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import javax.swing.*;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -12,6 +21,12 @@ public class Controller implements Initializable {
     private loginModel loginmodel = new loginModel();
     @FXML
     private Label dbStatus;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Button btnLogin;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -22,4 +37,35 @@ public class Controller implements Initializable {
         this.dbStatus.setText("Not Connected to DB");
     }
     }//initialize
+    @FXML
+    public void Login(ActionEvent event){
+        try{
+            if (this.loginmodel.isLogin(this.username.getText(),this.password.getText())){
+                Stage stage = (Stage) this.btnLogin.getScene().getWindow();
+                stage.close();
+
+                adminLogin();
+            }else {
+                JOptionPane.showMessageDialog(null,"Your username or" +
+                "password is not corrected.");
+            }
+
+        }catch (Exception localException){
+            localException.printStackTrace();
+        }
+
+    }//Login
+
+    private void adminLogin() {
+        try {
+            Stage adminStage = new Stage();
+            FXMLLoader adminLoader = new FXMLLoader();
+            Pane adminRoot = (Pane) adminLoader.load(getClass().getResource
+                    ("/Admin/AdminDashBoard.fxml").openStream());
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+
 }
